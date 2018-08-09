a1842543e2c9a027bae673004cf2dc150d4ca18e
Igor Starodynov <macht59@gmail.com>
2018-08-09 14:44:20 +0300

    Initial commit

---
+++ QuantityPick.Tests/QuantityPickTestFixture.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuantityPick.Tests
+{
+	[TestFixture]
+	public class QuantityPickTestFixture
+	{
+		[Test]
+		public void PickFromOneExactQuantity()
+		{
+			var qp = new QuantityPick(3, new List<decimal> { 3 });
+			var resultSet = qp.FindBestSummands();
+
+			Assert.That(resultSet, Is.Not.Null);
+			Assert.That(resultSet, Is.Not.Empty);
+			Assert.That(resultSet[0], Is.EqualTo(3));
+		}
+
+		[Test]
+		public void PickFromEmptyListReturnsNull()
+		{
+			var qp = new QuantityPick(3, new List<decimal>());
+			var resultSet = qp.FindBestSummands();
+
+			Assert.That(resultSet, Is.Null);
+		}
+
+		[Test]
+		public void CanPickFromTwoExactSummands()
+		{
+			var qp = new QuantityPick(3, new List<decimal> { 2, 1 });
+			var resultSet = qp.FindBestSummands();
+
+			Assert.That(resultSet, Is.Not.Null);
+			Assert.That(resultSet.Count, Is.EqualTo(2));
+			Assert.That(resultSet[0], Is.EqualTo(2));
+			Assert.That(resultSet[1], Is.EqualTo(1));
+		}
+
+		[Test]
+		public void CanNotPickSummandsWhenQuantitiesAreNotSuitable()
+		{
+			var qp = new QuantityPick(3, new List<decimal> { 2, 2 });
+			var resultSet = qp.FindBestSummands();
+
+			Assert.That(resultSet, Is.Null);
+		}
+
+		[Test]
+		public void CanPickBestSummandsWhenThereAreMoreThanOnePossibleSummandsCombination()
+		{
+			var qp = new QuantityPick(5, new List<decimal> { 2, 2, 1, 4, 6, 0 });
+			var resultSet = qp.FindBestSummands();
+
+			Assert.That(resultSet, Is.Not.Null);
+			Assert.That(resultSet.Count, Is.EqualTo(2));
+			Assert.That(resultSet.Any(x => x == 1));
+			Assert.That(resultSet.Any(x => x == 4));
+		}
+	}
+}
---
+++ QuantityPick/QuantityPick.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace QuantityPick
+{
+	public class QuantityPick
+	{
+		private readonly decimal _desiredSumm;
+		private readonly List<decimal> _quantitiesToPickFrom;
+
+		public QuantityPick(decimal desiredSumm, List<decimal> quantities)
+		{
+			_desiredSumm = desiredSumm;
+			_quantitiesToPickFrom = quantities;
+		}
+
+		public List<decimal> FindBestSummands()
+		{
+			var pickedList = new List<decimal>();
+			return FindBestSummandsRecursive(_desiredSumm, _quantitiesToPickFrom, pickedList);
+		}
+
+		private List<decimal> FindBestSummandsRecursive(decimal summ, List<decimal> qtyListToPickFrom, List<decimal> pickedQtyList)
+		{
+			if (summ == 0)
+			{
+				return pickedQtyList;
+			}
+			else if (summ < 0)
+			{
+				return null;
+			}
+			else
+			{
+				var resultList = new List<List<decimal>>();
+				foreach (var qty in qtyListToPickFrom.Where(x => x <= summ))
+				{
+
+					var originalListClone = qtyListToPickFrom.ToList();
+					var pickedQtyListClone = pickedQtyList.ToList();
+					originalListClone.Remove(qty);
+					pickedQtyListClone.Add(qty);
+					resultList.Add(FindBestSummandsRecursive(summ - qty, originalListClone, pickedQtyListClone));
+				}
+
+				return resultList.Where(x => x != null).Where(x => x.Count == resultList.Min(y => y.Count)).FirstOrDefault();
+			}
+		}
+	}
+}
